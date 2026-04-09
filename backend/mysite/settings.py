f00b213e27e62dd8de70f535663b59ac1873fafc
normalframe1094 <kapol2990@gmail.com>
2026-04-09 14:52:40 +0900

    fix: backend requirements 수정

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -178,6 +178,7 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "static"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 # =========================================================
 # FastAPI 서버 주소
