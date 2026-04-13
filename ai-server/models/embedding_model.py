025de19e776d0ab9628ae9e6c5939e84a5f78a45
normalframe1094 <kapol2990@gmail.com>
2026-04-13 17:32:51 +0900

    [fix] Delay Problem Solve

--- ai-server/models/embedding_model.py
+++ ai-server/models/embedding_model.py
@@ -1,6 +1,7 @@
 from sentence_transformers import SentenceTransformer
+import os
 
-# # 전역에서 1회 로드
-embedding_model = SentenceTransformer("upskyy/e5-small-korean")
-
-# embedding_model = None
+if os.getenv("CI") == "true":  # GitHub Actions는 CI=true를 자동으로 주입
+    embedding_model = None
+else:
+    embedding_model = SentenceTransformer("upskyy/e5-small-korean")
